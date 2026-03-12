488de5d53b073483eceea4d41efebebd1add4210
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-03-12 10:07:16 +0000

    lecture2 entry test

--- students/36180/lecture_2_entry_test.py
+++ students/36180/lecture_2_entry_test.py
@@ -2,7 +2,7 @@ my_Birthday = 20
 print(my_Birthday)
 print(type(my_Birthday))
 
-my_name = batuhan 
+my_name = "batuhan"
 print(my_name)
 print(type(my_name))
 
@@ -10,5 +10,6 @@ float_number = 3.5
 print(float_number)
 print(type(float_number))
 
-new_number = int(float_number)p
+new_number = int(float_number)
 print(new_number)
+
