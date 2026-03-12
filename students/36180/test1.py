3d3ed68bc2cab43141facea844b69e2b55770b87
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-03-12 09:33:52 +0000

    git add . git commit -m 'My comment' git push origin main

---
+++ students/36180/test1.py
@@ -0,0 +1,15 @@
+# this is my first code
+print("hello world")
+
+num = 5 
+
+if num > 5:
+  print("bigger than num")
+else :
+  print("smaller than num") 
+
+print("-----------------------------")
+
+animals =["aslan, kedi, yilan"]
+for i in animals:
+    print(i)
