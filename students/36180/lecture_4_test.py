ae606d28a1299b64984ea53418fe4b58184905a6
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-03-26 10:24:44 +0000

    test4

--- students/36180/lecture_4_test.py
+++ students/36180/lecture_4_test.py
@@ -1,1 +1,18 @@
 
+my_favorite_food = "kebab"
+
+class car : 
+    def __init__ (self ,brand,size):
+        self.brand = brand
+        self.size = size  
+        self.isworking = False 
+
+
+c1 = car("mercedes","5",False)
+print(t1)
+
+
+MIN_PASSWORD_LENGTH = 8
+
+#this is comment line 
+# x = [1,2,3]
