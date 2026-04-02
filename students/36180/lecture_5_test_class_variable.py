4bd51018654d476c916ca45c8a2a9c04810ede6d
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-04-02 08:40:22 +0000

    variable

--- students/36180/lecture_5_test_class_variable.py
+++ students/36180/lecture_5_test_class_variable.py
@@ -1,1 +1,13 @@
+class Smartphone:
+    
+    def __init__(self, brand, model, battery_capacity):
+        self.brand = brand              #
+        self.model = model             
+        self.battery = battery_capacity 
 
+
+my_phone = Smartphone("Apple", "iPhone 15", 3349)
+
+print(f"Brand: {my_phone.brand}")
+print(f"Model: {my_phone.model}")
+print(f"Battery: {my_phone.battery} mAh")
