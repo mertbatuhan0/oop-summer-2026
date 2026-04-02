6143aaa49c749bad387d2bf6f2ef685a7a762c80
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-04-02 08:53:06 +0000

    attributes

--- students/36180/lecture_5_test_with_attributes.py
+++ students/36180/lecture_5_test_with_attributes.py
@@ -1,1 +1,12 @@
+class Smartphone:
+    def __init__(self, brand, model, storage_gb):
+      
+        self.brand = brand
+        self.model = model
+        self.storage_gb = storage_gb
 
+phone_obj = Smartphone("Apple", "iPhone 15", 256)
+
+print(f"Brand: {phone_obj.brand}")
+print(f"Model: {phone_obj.model}")
+print(f"Storage: {phone_obj.storage_gb}GB")
